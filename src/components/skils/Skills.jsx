f3dff936d7320d28ce09694f9fe693f7a28003b3
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2023-02-06 00:31:51 -0500

    Subiendo estilos

--- src/components/skils/Skills.jsx
+++ src/components/skils/Skills.jsx
@@ -48,12 +48,7 @@ function Skills() {
           Tecnologías
         </h1>
         <ul className="skills">
-        {/* <div id="contenedor">
-                  <div id="barra">
-                    <div id="texto"></div>
-                  </div>
-                </div> */}
-
+    
           <li>
           <BootstrapTooltip
               leaveDelay={200}
@@ -73,7 +68,25 @@ function Skills() {
                 </div> */}
           </li>
 
-         
+          <li>
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title=" El CSS (hojas de estilo en cascada) es un lenguaje que define la apariencia de un documento escrito en un lenguaje de marcado (por ejemplo, HTML)."
+              followCursor
+              arrow
+            >
+            <img className="skill3" src={css} alt="logo html" />
+            </BootstrapTooltip>
+               {/* <div id="contenedor">
+                  <div id="barra">
+                    <div id="texto"></div>
+                  </div>
+                </div> */}
+          </li>
+
+
           <li>
             <BootstrapTooltip
               leaveDelay={200}
@@ -93,22 +106,75 @@ function Skills() {
           </li>
 
           <li>
-            <Tooltips title="eeeee">
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title=" Python es un lenguaje de programación ampliamente utilizado en las aplicaciones web, el desarrollo de software, la ciencia de datos y el machine learning (ML)."
+              followCursor
+              arrow
+            >
               <img className="skill4" src={python} alt="logo python" />
-            </Tooltips>
+            </BootstrapTooltip>
           </li>
+
+
           <li>
-            <img className="skill5" src={react} alt="logo react" />
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title="React es una biblioteca Javascript de código abierto diseñada para crear interfaces de usuario con el objetivo de facilitar el desarrollo de aplicaciones en una sola página."
+              followCursor
+              arrow
+            >
+               <img className="skill5" src={react} alt="logo react" />
+            </BootstrapTooltip>
           </li>
+
+
           <li>
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title="Bootstrap es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web."
+              followCursor
+              arrow
+            >
             <img className="skill6" src={boostrap} alt="logo boostrap" />
+            </BootstrapTooltip>
           </li>
+
+
           <li>
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title="Trello es un software de administración de proyectos con interfaz web y con cliente para iOS y android para organizar proyectos.​"
+              followCursor
+              arrow
+            >
             <img className="skill7" src={trello} alt="logo trello" />
+            </BootstrapTooltip>
           </li>
+
+
           <li>
+          <BootstrapTooltip
+              leaveDelay={200}
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 600 }}
+              title="Figma es un editor de gráficos vectorial y una herramienta de generación de prototipos, principalmente basada en la web, con características off-line adicionales habilitadas por aplicaciones de escritorio en macOS y Windows."
+              followCursor
+              arrow
+            >
             <img className="skill8" src={figma} alt="logo figma" />
+            </BootstrapTooltip>
           </li>
+
+          
         </ul>
       </div>
     </>
